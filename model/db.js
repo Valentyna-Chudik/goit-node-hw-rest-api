9006f4e1721f6064f2ccdcd5a9845d22439bf7fe
Valentyna-Chudik <valentyna_chudik@ukr.net>
2021-02-20 23:27:36 +0200

    Func to work with db fixed; Routes, Validation added

--- model/db.js
+++ model/db.js
@@ -1,9 +1,9 @@
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
+// const low = require("lowdb");
+// const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("./model/contacts.json");
-const db = low(adapter);
+// const adapter = new FileSync("./model/contacts.json");
+// const db = low(adapter);
 
-db.defaults({ contacts: [] }).write();
+// db.defaults({ contacts: [] }).write();
 
-module.exports = db;
+// module.exports = db;
